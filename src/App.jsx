e1619b0e41eb2bd7ccc3423cf8d5a09f8e91e0f8
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-12 17:45:23 +0100

    add genre filtering for movies array

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,20 @@ function App() {
   ]
 
   // stati per il filtro
-  const [genre, setGenre] = useState('')
+  const [selectedGenre, setSelectedGenre] = useState('')
   const [filteredMovies, setFilteredMovies] = useState(movies)
+
+  useEffect(() => {
+    if (selectedGenre === "") {
+      // se non si seleziona il genere, mostra tutti i film
+      setFilteredMovies(movies)
+    } else {
+      // altrimenti filtra
+      const filtered = movies.filter(movie => movie.genre === selectedGenre)
+      setFilteredMovies(filtered)
+    }
+  }, [selectedGenre]); // useEffect si attiva al cambiare di selectedGenre
+  // quindi mostra tutto se la stringa selectedGenre è vuota, altrimenti filtra l'array movies per il genere selezionato
 
   return (
     <div className='container'>
